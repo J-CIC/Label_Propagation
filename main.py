98c8f22d8e6500ca8683bc4f1a992dbaeac45841
J-CIC <791863347@qq.com>
2018-06-06 21:42:47 +0800

    modify main.py

--- main.py
+++ main.py
@@ -15,8 +15,6 @@ def generate_weight_graph(graph_array,max_id,a):
             weight_graph[j][i] = weight_graph[i][j]
     return weight_graph
 
-def main():
-    pass
 edge_link = pd.read_table("email-Eu-core.txt",' ',header=None)
 labels_res = pd.read_table("email-Eu-core-department-labels.txt"," ",header=None)
 # label_class = 42 # from 0 to 41
@@ -32,41 +30,51 @@ weight_graph = generate_weight_graph(graph_array,max_id,1)
 for i in range(0,max_id):
     weight_graph[i] = weight_graph[i]/weight_graph[i].sum()
 
-train=labels_res.sample(frac=0.50)
-test=labels_res.drop(train.index)
 
-matrix = np.zeros((max_id,label_class))
-for index,row in train.iterrows():
-    matrix[index][row[1]] = 1
+def loop(frac):
+    sum = 0.0
+    for i in range(0,10):
+        sum = sum + main(frac)
+    print("Average result of frac %f is %f "% (frac,sum/10))
 
-for index,row in test.iterrows():
-    matrix[index][random.randint(0,label_class-1)] = 1
+def main(frac):
+    train=labels_res.sample(frac=frac)
+    test=labels_res.drop(train.index)
 
-iter_count = 0
-while(True):
-    label_true=list()
-    label_predict = list()
-    t_matrix = np.dot(weight_graph,matrix)
-    count = 0
+    matrix = np.zeros((max_id,label_class))
     for index,row in train.iterrows():
-        t_matrix[index].fill(0)
-        t_matrix[index][row[1]] = 1
+        matrix[index][row[1]] = 1
 
     for index,row in test.iterrows():
-        idx = t_matrix[index].argmax()
-        idx2 = matrix[index].argmax()
-        label_true.append(row[1])
-        label_predict.append(idx)
-        t_matrix[index].fill(0)
-        t_matrix[index][idx] = 1
-        if(idx!=idx2):
-            count = count + 1
+        matrix[index][random.randint(0,label_class-1)] = 1
+    iter_count = 0
+    while(True):
+        label_true=list()
+        label_predict = list()
+        t_matrix = np.dot(weight_graph,matrix)
+        count = 0
+        for index,row in train.iterrows():
+            t_matrix[index].fill(0)
+            t_matrix[index][row[1]] = 1
 
-    matrix = t_matrix
-    iter_count = iter_count +1
-    print("iter %d:"%iter_count, " diff count:",count)
-    if(count==0):
-        break
+        for index,row in test.iterrows():
+            idx = t_matrix[index].argmax()
+            idx2 = matrix[index].argmax()
+            label_true.append(row[1])
+            label_predict.append(idx)
+            t_matrix[index].fill(0)
+            t_matrix[index][idx] = 1
+            if(idx!=idx2):
+                count = count + 1
+
+        matrix = t_matrix
+        iter_count = iter_count +1
+        # print("iter %d:"%iter_count, " diff count:",count)
+        if(count==0):
+            break
+    result = rand_index_score(label_true,label_predict)
+    print("Converge after %d iterations" %iter_count," Final Rand index score of frac %f"%frac,result)
+    return result
 # print(label_true)
 # print(label_predict)
 def rand_index_score(clusters, classes):
@@ -80,10 +88,13 @@ def rand_index_score(clusters, classes):
     tn = comb(len(A), 2) - tp - fp - fn
     return (tp + tn) / (tp + fp + fn + tn)
 
-print(rand_index_score(label_true,label_predict))
 
 
 
 
 if __name__ == '__main__':
-    main()
+    loop(0.05)
+    loop(0.10)
+    loop(0.15)
+    loop(0.20)
+    loop(0.25)
